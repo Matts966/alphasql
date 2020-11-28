fb35bed035754c4d035c070bf7c9451019b4557e
Matts966 <Matts966@users.noreply.github.com>
2020-11-28 17:57:07 +0900

    Support _TABLE_SUFFIX in the sample CI

--- samples/sample-ci/python_entrypoint.py
+++ samples/sample-ci/python_entrypoint.py
@@ -5,31 +5,38 @@ import json
 
 
 def install(package):
-    if hasattr(pip, 'main'):
-        pip.main(['install', package])
+    if hasattr(pip, "main"):
+        pip.main(["install", package])
     else:
-        pip._internal.main(['install', package])
+        pip._internal.main(["install", package])
+
 
 def main():
     from google.cloud.bigquery import Client
-    TABLE_SEP = '.'
+
     tables = {}
-    with open('/vol/required_tables.txt') as rt:
+    with open("/vol/required_tables.txt") as rt:
         table_names = rt.read().split()
         bq_client = Client()
         for table_name in table_names:
-            splited_table_name = table_name.split('.')
+            splited_table_name = table_name.split(".")
             if len(splited_table_name) == 3:
-                dataset_ref = bq_client.dataset(splited_table_name[1], project=splited_table_name[0])
+                dataset_ref = bq_client.dataset(
+                    splited_table_name[1], project=splited_table_name[0]
+                )
             else:
                 dataset_ref = bq_client.dataset(splited_table_name[0])
             table_ref = dataset_ref.table(splited_table_name[-1])
             table = bq_client.get_table(table_ref)
             tables[table_name] = [field.to_api_repr() for field in table.schema]
-    with open('/vol/schema.json', mode='w') as schema:
+            if table_name.endswith("*"):
+                tables[table_name].append(
+                    {"name": "_TABLE_SUFFIX", "type": "STRING", "mode": "REQUIRED"}
+                )
+    with open("/vol/schema.json", mode="w") as schema:
         schema.write(json.dumps(tables))
 
 
-if __name__ == '__main__':
-    install('google-cloud-bigquery')
+if __name__ == "__main__":
+    install("google-cloud-bigquery")
     main()
