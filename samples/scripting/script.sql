c18f77877f1d134c8311ea14962f64af729377c0
Matts966 <Matts966@users.noreply.github.com>
2022-03-28 04:25:32 +0900

    CAST

--- samples/scripting/script.sql
+++ samples/scripting/script.sql
@@ -6,7 +6,7 @@ BEGIN
     tablename1;
   CREATE OR REPLACE TABLE datawarehouse1 AS
   SELECT
-    column2 AS x
+    CAST(column2 AS INT64) AS x
   FROM
     tablename2;
   CALL create_datawarehouse3();
