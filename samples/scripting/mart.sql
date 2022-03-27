5b68e49d91a1c1f9fae1a7ed19f60a31566ee674
Matts966 <Matts966@users.noreply.github.com>
2022-03-28 00:16:25 +0900

    Use `

--- samples/scripting/mart.sql
+++ samples/scripting/mart.sql
@@ -7,7 +7,7 @@ SELECT * FROM datawarehouse1
 UNION ALL
 SELECT * FROM datawarehouse2
 UNION ALL
-SELECT * FROM datawarehouse3;
+SELECT * FROM `datawarehouse3`;
 
 DROP TABLE IF EXISTS `datawarehouse1`;
 DROP TABLE IF EXISTS `datawarehouse2`;
